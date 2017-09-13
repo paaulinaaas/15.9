79dd85b2bbd4c1bc1f18f23e49b522a092dd2935
Michal Gacek <michal.gacek@geomatyka-krakow.pl>
2017-09-13 09:07:24 +0200

    Fixes

--- build.js
+++ build.js
@@ -8,75 +8,32 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+var User = function (_React$Component) {
+  _inherits(User, _React$Component);
 
-var App = function (_React$Component) {
-  _inherits(App, _React$Component);
+  function User() {
+    _classCallCheck(this, User);
 
-  function App() {
-    _classCallCheck(this, App);
-
-    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
-
-    _this.state = {
-      searchText: '',
-      users: []
-    };
-    return _this;
+    return _possibleConstructorReturn(this, (User.__proto__ || Object.getPrototypeOf(User)).apply(this, arguments));
   }
 
-  _createClass(App, [{
-    key: 'onChangeHandle',
-    value: function onChangeHandle(event) {
-      this.setState({ searchText: event.target.value });
-    }
-  }, {
-    key: 'onSubmit',
-    value: function onSubmit(event) {
-      var _this2 = this;
-
-      event.preventDefault();
-      var searchText = this.state.searchText;
-
-      var url = 'https://api.github.com/search/users?q=' + searchText;
-      fetch(url).then(function (response) {
-        return response.json();
-      }).then(function (responseJson) {
-        return _this2.setState({ users: responseJson.items });
-      });
-    }
-  }, {
+  _createClass(User, [{
     key: 'render',
     value: function render() {
-      var _this3 = this;
-
       return React.createElement(
         'div',
         null,
+        React.createElement('img', { src: this.props.user.avatar_url, style: { maxWidth: '100px' } }),
         React.createElement(
-          'form',
-          { onSubmit: function onSubmit(event) {
-              return _this3.onSubmit(event);
-            } },
-          React.createElement(
-            'label',
-            { htmlFor: 'searchText' },
-            'Search by user name'
-          ),
-          React.createElement('input', {
-            type: 'text',
-            id: 'searchText',
-            onChange: function onChange(event) {
-              return _this3.onChangeHandle(event);
-            },
-            value: this.state.searchText })
-        ),
-        React.createElement(UsersList, { users: this.state.users })
+          'a',
+          { href: this.props.user.html_url, target: '_blank' },
+          this.props.user.login
+        )
       );
     }
   }]);
 
-  return App;
+  return User;
 }(React.Component);
 
 var UsersList = function (_React$Component2) {
@@ -109,30 +66,73 @@ var UsersList = function (_React$Component2) {
   return UsersList;
 }(React.Component);
 
-var User = function (_React$Component3) {
-  _inherits(User, _React$Component3);
+var App = function (_React$Component3) {
+  _inherits(App, _React$Component3);
 
-  function User() {
-    _classCallCheck(this, User);
+  function App() {
+    _classCallCheck(this, App);
 
-    return _possibleConstructorReturn(this, (User.__proto__ || Object.getPrototypeOf(User)).apply(this, arguments));
+    var _this3 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this));
+
+    _this3.state = {
+      searchText: '',
+      users: []
+    };
+    return _this3;
   }
 
-  _createClass(User, [{
+  _createClass(App, [{
+    key: 'onChangeHandle',
+    value: function onChangeHandle(event) {
+      this.setState({ searchText: event.target.value });
+    }
+  }, {
+    key: 'onSubmit',
+    value: function onSubmit(event) {
+      var _this4 = this;
+
+      event.preventDefault();
+      var searchText = this.state.searchText;
+
+      var url = 'https://api.github.com/search/users?q=' + searchText;
+      fetch(url).then(function (response) {
+        return response.json();
+      }).then(function (responseJson) {
+        return _this4.setState({ users: responseJson.items });
+      });
+    }
+  }, {
     key: 'render',
     value: function render() {
+      var _this5 = this;
+
       return React.createElement(
         'div',
         null,
-        React.createElement('img', { src: this.props.user.avatar_url, style: { maxWidth: '100px' } }),
         React.createElement(
-          'a',
-          { href: this.props.user.html_url, target: '_blank' },
-          this.props.user.login
-        )
+          'form',
+          { onSubmit: function onSubmit(event) {
+              return _this5.onSubmit(event);
+            } },
+          React.createElement(
+            'label',
+            { htmlFor: 'searchText' },
+            'Search by user name'
+          ),
+          React.createElement('input', {
+            type: 'text',
+            id: 'searchText',
+            onChange: function onChange(event) {
+              return _this5.onChangeHandle(event);
+            },
+            value: this.state.searchText })
+        ),
+        React.createElement(UsersList, { users: this.state.users })
       );
     }
   }]);
 
-  return User;
+  return App;
 }(React.Component);
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
